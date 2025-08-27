2fbb2fce543ecc32c6357103ed59eea2e5959cb6
PaulOR26 <pj_orourke@outlook.com>
2025-08-27 09:17:22 +0100

    add filtering

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,115 @@
+import { ChangeEvent, useEffect, useState } from 'react';
+import { LIMIT } from './constants';
 import { useRepos } from './useRepos';
-import { useState } from 'react';
 
 import './App.css';
 
+const today = new Date();
+const threeMonthsAgo = new Date();
+threeMonthsAgo.setMonth(today.getMonth() - 3);
+
+const formatDate = (date: Date) => date.toISOString().split('T')[0];
+
 function App() {
+  const [createdFrom, setCreatedFrom] = useState(formatDate(threeMonthsAgo));
+  const [createdTo, setCreatedTo] = useState(formatDate(today));
+  const [orderBy, setOrderBy] = useState('desc');
+  const [page, setPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const [sortBy, setSortBy] = useState('stars');
-  const [orderBy, setOrderBy] = useState('desc');
-  const [limit, setLimit] = useState('10');
-  const [page, setPage] = useState('1');
 
   const { repos, repoCount, isLoading, error } = useRepos({
+    createdFrom,
+    createdTo,
+    orderBy,
+    page,
     searchTerm,
     sortBy,
-    orderBy,
-    limit,
-    page,
   });
+
+  const totalPages = Math.ceil(Math.min(repoCount / LIMIT, 1000 / LIMIT));
+
+  const handleChangeCreatedFrom = (event: ChangeEvent<HTMLInputElement>) =>
+    setCreatedFrom(event.target.value);
+
+  const handleChangeCreatedTo = (event: ChangeEvent<HTMLInputElement>) =>
+    setCreatedTo(event.target.value);
+
+  const handleChangeOrderBy = (event: ChangeEvent<HTMLSelectElement>) =>
+    setOrderBy(event.target.value);
+
+  const handleChangeSearch = (event: ChangeEvent<HTMLInputElement>) =>
+    setSearchTerm(event.target.value);
+
+  const handleChangeSortBy = (event: ChangeEvent<HTMLSelectElement>) =>
+    setSortBy(event.target.value);
+
+  const handleLoadMore = () => setPage((curr) => curr + 1);
+
+  useEffect(() => {
+    setPage(1);
+  }, [searchTerm, sortBy, orderBy, createdFrom, createdTo]);
 
   return (
     <div>
-      <header></header>
+      <header>Github API</header>
+
+      <main>
+        <label htmlFor='search-input'>Search</label>
+
+        <input
+          id='search-input'
+          onChange={handleChangeSearch}
+          placeholder='Enter search term'
+          value={searchTerm}
+        />
+
+        <label>Created</label>
+
+        <label htmlFor='from-date'>From</label>
+
+        <input
+          id='from-date'
+          onChange={handleChangeCreatedFrom}
+          type='date'
+          value={createdFrom}
+        />
+
+        <label htmlFor='to-date'>To</label>
+
+        <input
+          id='to-date'
+          onChange={handleChangeCreatedTo}
+          type='date'
+          value={createdTo}
+        />
+
+        <label htmlFor='sort-select'>Sort</label>
+
+        <select id='sort-select' onChange={handleChangeSortBy} value={sortBy}>
+          <option value='stars'>Stars</option>
+          <option value='forks'>Forks</option>
+        </select>
+
+        <label htmlFor='order-select'>Order</label>
+
+        <select
+          id='order-select'
+          onChange={handleChangeOrderBy}
+          value={orderBy}
+        >
+          <option value='desc'>Highest to Lowest</option>
+          <option value='asc'>Lowest to Highest</option>
+        </select>
+
+        {page < totalPages ? (
+          <button disabled={isLoading} onClick={handleLoadMore}>
+            {isLoading ? 'Loading...' : 'Load More'}
+          </button>
+        ) : (
+          <p>No more results</p>
+        )}
+      </main>
     </div>
   );
 }
--- src/useRepos.ts
+++ src/useRepos.ts
@@ -1,22 +1,25 @@
+import { LIMIT } from './constants';
 import { useState, useEffect } from 'react';
 
 interface UseReposParams {
-  limit: string;
+  createdFrom: string;
+  createdTo: string;
   orderBy: string;
-  page: string;
+  page: number;
   searchTerm: string;
   sortBy: string;
 }
 
 export const useRepos = ({
+  createdFrom,
+  createdTo,
+  orderBy,
+  page,
   searchTerm,
   sortBy,
-  orderBy,
-  limit,
-  page,
 }: UseReposParams) => {
   const [repos, setRepos] = useState([]);
-  const [repoCount, setRepoCount] = useState([]);
+  const [repoCount, setRepoCount] = useState(0);
   const [isLoading, setIsLoading] = useState(false);
   const [error, setError] = useState('');
 
@@ -30,12 +33,17 @@ export const useRepos = ({
 
     try {
       const response = await fetch(
-        `https://api.github.com/search/repositories?q=${parsedSearchTerm}&sort=${sortBy}&order=${orderBy}&per_page=${limit}&page=${page}`
+        `https://api.github.com/search/repositories?q=${parsedSearchTerm}+created:${createdFrom}..${createdTo}&sort=${sortBy}&order=${orderBy}&per_page=${LIMIT}&page=${page}`
       );
       const { items, total_count } = await response.json();
 
-      setRepos(items || []);
-      setRepoCount(total_count);
+      if (items) {
+        setRepos((curr) => (page === 1 ? items : [...curr, ...items]));
+        setRepoCount(total_count);
+      } else {
+        setRepos([]);
+        setRepoCount(0);
+      }
     } catch (err) {
       setError('Failed to fetch repositories');
     } finally {
@@ -44,14 +52,10 @@ export const useRepos = ({
   };
 
   useEffect(() => {
-    fetchRepos();
-  }, [sortBy, orderBy, limit, page]);
-
-  useEffect(() => {
     const delayDebounce = setTimeout(fetchRepos, 500);
 
     return () => clearTimeout(delayDebounce);
-  }, [searchTerm]);
+  }, [createdFrom, createdTo, orderBy, page, searchTerm, sortBy]);
 
   return { repos, repoCount, isLoading, error };
 };
